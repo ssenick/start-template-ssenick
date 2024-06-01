8d39184ac75428a4d8a3beced460e3b26543303e
ssenick <ssenick@ukr.net>
2024-05-31 19:50:04 -0600

    added declares

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg' {
+   import { type FC, type SVGProps } from 'react';
+   const content: FC<SVGProps<SVGElement>>;
+   export default content;
+}
