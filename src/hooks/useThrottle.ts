32bb7d9b75c06b273021afe2eb4061afb12e3822
ssenick <ssenick@ukr.net>
2024-06-04 20:42:38 -0600

    hooks useThrottle, useDebounce

--- src/hooks/useThrottle.ts
+++ src/hooks/useThrottle.ts
@@ -1,27 +1,85 @@
 import { useCallback, useEffect, useRef } from 'react';
 
-export function useThrottle<T extends unknown[]>(callback: (...args: T[]) => void, delay: number) {
-   const throttleRef = useRef<null | boolean>(null);
-   const throttleTimeout = useRef<number>(0);
+type CallbackFunction<T extends unknown[]> = (...args: T) => void;
+
+interface UseThrottleProps<T extends unknown[]> {
+   callback: CallbackFunction<T>;
+   delay: number;
+}
+
+export function useThrottle<T extends unknown[]>({ callback, delay }: UseThrottleProps<T>) {
+   const throttleRef = useRef<boolean | null>(null);
+   const throttleTimeout = useRef<ReturnType<typeof setTimeout> | null>(null);
+
    const throttledCallback = useCallback(
-      (...args: T[]) => {
+      (..._args: T) => {
          if (!throttleRef.current) {
-            callback(...args);
+            callback(..._args);
             throttleRef.current = true;
             throttleTimeout.current = setTimeout(() => {
                throttleRef.current = false;
             }, delay);
          }
       },
-      [delay, callback],
+      [callback, delay],
    );
 
    useEffect(() => {
       return () => {
          if (throttleTimeout.current) {
-            clearTimeout(throttleTimeout.current);
+            clearTimeout(throttleTimeout.current as number);
          }
       };
    }, []);
    return throttledCallback;
 }
+
+// EXAMPLES
+
+// ONE argument
+// const [inputValue, setInputValue] = useState('');
+//
+// const throttledLog = useDebounce<[string]>({
+//    callback: (value) => {
+//       console.log('Throttled value:', value);
+//    },
+//    delay: 1000,
+// });
+//
+// const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+//    const value = event.target.value;
+//    setInputValue(value);
+//    throttledLog(value); // Передаем значение в throttle-функцию
+// };
+
+// TWO argument
+// const [inputValue, setInputValue] = useState('');
+//
+// const throttledLog = useDebounce<[string, number]>({
+//    callback: (value, index) => {
+//       console.log('Throttled value:', value, 'index:', index);
+//    },
+//    delay: 1000,
+// });
+//
+// const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+//    const value = event.target.value;
+//    setInputValue(value);
+//    throttledLog(value, 2); // Передаем значение в throttle-функцию
+// };
+
+// NO argument
+// const [inputValue, setInputValue] = useState('');
+//
+// const throttledLog = useDebounce({
+//    callback: () => {
+//       console.log('Throttled value');
+//    },
+//    delay: 1000,
+// });
+//
+// const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+//    const value = event.target.value;
+//    setInputValue(value);
+//    throttledLog();
+// };
