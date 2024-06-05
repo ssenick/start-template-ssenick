7ea08c3faf37c510e7babaab57033fc26e090966
ssenick <ssenick@ukr.net>
2024-06-04 20:45:44 -0600

    added react-responsive,MediaQueryProvider, useMediaQueryValues, fix screen height on mobile devices

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,24 @@
 import './styles/index.scss';
 
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 
 import { AppRouter } from '@/app/povaiders/Router';
 import { Header } from '@/components/Header';
 import { classNames } from '@/helpers/classNames/classNames';
+import { fixHeight } from '@/helpers/func/fixHeight';
+import { useMediaQueryValues } from '@/hooks/useMediaQueryValues';
 
 function App() {
+   const { isMobile } = useMediaQueryValues();
+
+   useEffect(() => {
+      if (isMobile) {
+         fixHeight();
+      }
+   }, [isMobile]);
+
    return (
-      <div className={classNames('app', {}, [])}>
+      <div className={classNames('app', { mobile: isMobile }, [])}>
          <Suspense fallback={''}>
             <Header className={'app__header'} />
             <AppRouter className={'app__main'} />
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/app/App.tsx';
+import { MediaQueryProvider } from '@/app/povaiders/MediaQueryProvaider';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to mount the application, check the container!');
@@ -12,6 +13,8 @@ const root = createRoot(container);
 
 root.render(
    <BrowserRouter>
-      <App />
+      <MediaQueryProvider>
+         <App />
+      </MediaQueryProvider>
    </BrowserRouter>,
 );
